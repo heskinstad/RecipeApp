33eb257133c0778e8384c278b8ec57d0a8e6a9a5
heskinstad <heskinstad@gmail.com>
2025-03-03 14:28:44 +0100

    Added Repository

---
+++ RecipeApp.API/Mapper/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace RecipeApp.API.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
---
+++ RecipeApp.API/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecipeApp.API.Data;
+
+namespace RecipeApp.API.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private RecipeContext _db;
+        private DbSet<T> _table = null!;
+
+        public Repository(RecipeContext db)
+        {
+            _db = db;
+            _table = _db.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> Get()
+        {
+            return _table.ToList();
+        }
+
+        public async Task<T> Insert(T entity)
+        {
+            _table.Add(entity);
+            _db.SaveChanges();
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _table.Attach(entity);
+            _db.Entry(entity).State = EntityState.Modified;
+            _db.SaveChanges();
+            return entity;
+        }
+
+        public async Task<T> Delete(object id)
+        {
+            T entity = _table.Find(id);
+            _table.Remove(entity);
+            _db.SaveChanges();
+            return entity;
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return _table.Find(id);
+        }
+    }
+}
